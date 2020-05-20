aa591539dd8ed6cb65c10a8d1edd86a1b0e13938
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-05-20 11:45:22 +0200

    Add interrupt routine for STRG-C

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <wiringPi.h>
+#include <signal.h>
+#include <iostream>
+
+/// Interrupt Routine for STRG-C
+void signalHandler(int signum)
+{
+    std::cout << "Strg-C Programmende" << std::endl;
+    // Beenden Sie hier bitte alle Verbindung zu den Sensoren etc.
+    exit(signum);
+}
+
 int main (void)
 {
+    // Csignal für Abbruch über STRG-C
+    signal(SIGINT, signalHandler);
     wiringPiSetup () ;
     pinMode (0, OUTPUT) ;
     for (;;)
