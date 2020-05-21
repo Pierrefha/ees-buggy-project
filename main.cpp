08f336f13a210b3e144434cfb9e41bbd1dca6646
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-05-21 13:25:07 +0200

    Add ncurses based wheeling through wasd

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <motor_engine/constants.h>
 #include <motor_engine/motor_engine.h>
+#include <curses.h>
 
 int fd = -1;
 motor_engine* engine = nullptr;
@@ -91,37 +92,104 @@ int main ()
     // wait for oscillator
     std::this_thread::sleep_for (std::chrono::milliseconds (5));
 
-    std::cout << "driving forward" << std::endl;
-    engine->set_speed(255);
-    engine->forward();
-    std::this_thread::sleep_for (std::chrono::milliseconds (500));
-    engine->smooth_stop();
+    bool auto_movement = false;
+    if(auto_movement){
+        std::cout << "driving forward" << std::endl;
+        engine->set_speed(500);
+        engine->forward();
+        std::this_thread::sleep_for (std::chrono::milliseconds (500));
+        engine->smooth_stop();
 
-    std::cout << "driving backwards" << std::endl;
-    engine->set_speed(500);
-    engine->backwards();
-    std::this_thread::sleep_for (std::chrono::milliseconds (500));
-    engine->smooth_stop();
+        std::cout << "driving backwards" << std::endl;
+        engine->set_speed(500);
+        engine->backwards();
+        std::this_thread::sleep_for (std::chrono::milliseconds (500));
+        engine->smooth_stop();
 
-    engine->release_engine();
-    delete engine;
-//
-//    //LEFT WHEEL
-//    int pwm = 8;
-//    int pin1 = 9;
-//    int pin2 = 10;
-//    //RIGHT WHEEl
-//    int pwmPin = 13;
-//    int in2Pin = 12;
-//    int in1Pin = 11;
-//
-//    setChannel (pwm, 0, 100 * 16);
-//    setPin (pin1, true);
-//    setPin (pin2, false);
-//
-//    std::this_thread::sleep_for(std::chrono::milliseconds (1000));
-//
-//    // release the motor after use
-//    setPin (pin1, false);
-//    setPin (pin2, false);
+        engine->release_engine();
+        delete engine;
+    }else{
+        char user_cmd = 0;
+        initscr();
+        cbreak();
+        noecho();
+        while((user_cmd = getch()) != 'q'){
+            switch (user_cmd){
+                case 'w':{
+                    switch(engine->direction()){
+                        case direction::IN_PLACE_TURN_RIGHT:
+                        case direction::IN_PLACE_TURN_LEFT:
+                        case direction::BACKWARDS:
+                            engine->smooth_stop();
+                            engine->forward();
+                            break;
+                        case direction::STOP:
+                            engine->forward();
+                            break;
+                        case direction::FORWARD:
+                            engine->increase_speed();
+                            break;
+                    }
+                    break;
+                }
+                case 's':{
+                    switch(engine->direction()){
+                        case direction::IN_PLACE_TURN_RIGHT:
+                        case direction::IN_PLACE_TURN_LEFT:
+                        case direction::FORWARD:
+                            engine->smooth_stop();
+                            engine->backwards();
+                            break;
+                        case direction::STOP:
+                            engine->backwards();
+                            break;
+                        case direction::BACKWARDS:
+                            engine->increase_speed();
+                            break;
+                    }
+                    break;
+                }
+                case 'd':{
+                    switch(engine->direction()){
+                        case direction::IN_PLACE_TURN_RIGHT:
+                            engine->increase_speed();
+                            break;
+                        case direction::IN_PLACE_TURN_LEFT:
+                            engine->smooth_stop();
+                            engine->turn_in_place_right();
+                            break;
+                        case direction::FORWARD:
+                        case direction::BACKWARDS:
+                            engine->turn_right();
+                            break;
+                        case direction::STOP:
+                            engine->turn_in_place_right();
+                            break;
+                    }
+                    break;
+                }
+                case 'a':{
+                    switch(engine->direction()){
+                        case direction::IN_PLACE_TURN_RIGHT:
+                            engine->smooth_stop();
+                            engine->turn_in_place_left();
+                            break;
+                        case direction::IN_PLACE_TURN_LEFT:
+                            engine->increase_speed();
+                            break;
+                        case direction::FORWARD:
+                        case direction::BACKWARDS:
+                            engine->turn_left();
+                            break;
+                        case direction::STOP:
+                            engine->turn_in_place_left();
+                            break;
+                    }
+                    break;
+                }
+                default: break;
+            }
+
+        }
+    }
 }
