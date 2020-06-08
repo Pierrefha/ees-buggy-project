a2139dabd9b54cf3c3306ecf2f1a3b2d369cdfa7
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-06-08 13:20:31 +0200

    Change this. to this->

--- src/ultrasonic_sensor/ultrasonic_sensor.cpp
+++ src/ultrasonic_sensor/ultrasonic_sensor.cpp
@@ -5,20 +5,20 @@
 #include <stdio.h>
 
 ultrasonic_sensor::ultrasonic_sensor(int8_t trigger_pin, int8_t echo_pin, int8_t brake_light_pin){    
-    this.trigger_pin = trigger_pin;
-    this.echo_pin = echo_pin;
-    this.brake_light_pin = brake_light_pin;
+    this->trigger_pin = trigger_pin;
+    this->echo_pin = echo_pin;
+    this->brake_light_pin = brake_light_pin;
 }
 
 void ultrasonic_sensor::init(){
     //set trigger pin to OUTPUT and LOW
-    pinMode(this.trigger_pin,OUTPUT);
-    digitalWrite(this.trigger_pin,LOW);
+    pinMode(this->trigger_pin,OUTPUT);
+    digitalWrite(this->trigger_pin,LOW);
     //set ECHO pin to INPUT MODE
-    pinMode(this.echo_pin,OUTPUT);
+    pinMode(this->echo_pin,OUTPUT);
     //set brake_light_pin to OUTPUT and LOW
-    pinMode(this.brake_light_pin,OUTPUT);
-    digitalWrite(this.brake_light_pin,LOW);
+    pinMode(this->brake_light_pin,OUTPUT);
+    digitalWrite(this->brake_light_pin,LOW);
 }
 
 
@@ -26,16 +26,16 @@ int32_t ultrasonic_sensor::get_distance(){
     double speed_of_sound = SPEED_OF_SOUND_IN_CM_PER_SECOND;
 
     //set trigger to high for one flank
-    digitalWrite(this.trigger_pin,HIGH);
+    digitalWrite(this->trigger_pin,HIGH);
     auto start = std::chrono::system_clock::now();
     //sleep for one millisecond
-    std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME_IN_MILLISECONDS0));
+    std::this->thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME_IN_MILLISECONDS0));
     //set trigger to low
-    digitalWrite(this.trigger_pin,LOW);
+    digitalWrite(this->trigger_pin,LOW);
 
     //poll echo pin until we receive response
     while(TRUE){
-        if(digitalRead(this.echo_pin)==HIGH){
+        if(digitalRead(this->echo_pin)==HIGH){
             auto end = std::chrono::system_clock::now();
             break;
         }
@@ -48,7 +48,7 @@ int32_t ultrasonic_sensor::get_distance(){
 }
 
 void ultrasonic_sensor::set_brake_light(int8_t mode){
-    digitalWrite(this.brake_light_pin,mode);
+    digitalWrite(this->brake_light_pin,mode);
 }
 
 //TODO: probably should be in engine cpp or wheel cpp
