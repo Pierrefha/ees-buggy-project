0918ffec31bcbb2c99d83d5b0ab0f87575e18666
Pierre Dahmani <pierre.dahmani@alumni.fh-aachen.de>
2020-06-13 23:23:19 +0200

    implement first version of emergency break

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include <motor_engine/motor_engine.h>
 #include <ultrasonic_sensor/ultrasonic_sensor.h>
 #include <curses.h>
+#include <thread>
+#include <chrono>
 
 #define ULTRASONIC_BRAKE_LIGHT_PIN_WPI 0
 #define ULTRASONIC_ECHO_PIN_WPI 1
@@ -32,171 +34,227 @@ void signalHandler(int signum)
     exit(signum);
 }
 
+
+/*
+ * Periodically checks distance using the ultrasonic sensor.
+ */
+void poll_distance(bool * end_ptr, ultrasonic_sensor * sensor, motor_engine * engine)
+{
+	double distance = 0;
+	while(true){
+		distance = sensor->calc_distance();
+		std::cout << "current distance: " << distance << std::endl;
+		// TODO set distance threshold depending on current speed
+		// Set stop condition to true if we are about 10 cm close to an object. 
+		if(distance <= 10){
+			*end_ptr=true;
+			sensor->set_brake_light(ON);
+			engine->emergency_stop();
+
+			/*
+			 * TODO TURN 45 degrees for a maximum of 4 times (maybe some cat just ran by)
+			 * If distance will be <=10 after one iteration we disable the brake light
+			 * and enable remote wasd control again.
+			 * otherwise we are stuck inside 4 walls.
+			 * and just quit with a "you shall not pass" message? xd
+			 */
+			 
+			 }
+			 else {
+			//TODO find better solution than turning on every iteration
+			sensor->set_brake_light(OFF);
+			}
+
+			// pause 100 ms
+			auto start = std::chrono::steady_clock::now();
+			auto delay = std::chrono::steady_clock::now();
+			while(delay - start < std::chrono::milliseconds(100)){
+			delay = std::chrono::steady_clock::now();
+			}
+	}
+}
+
+
 int main ()
 {
-    // Csignal für Abbruch über STRG-C
-    signal(SIGINT, signalHandler);
-    //init wiringPi
-    wiringPiSetup () ;
+	bool end = false;
+	bool * end_ptr = & end;
+	// Csignal für Abbruch über STRG-C
+	signal(SIGINT, signalHandler);
+	//init wiringPi
+	wiringPiSetup () ;
 
-    //test blink led
-    /*
-    pinMode (0, OUTPUT) ;
-    do
-    {
-        digitalWrite (0, HIGH) ; delay (1000) ;
-        digitalWrite (0,  LOW) ; delay (1000) ;
-    }while(true);
-    */
+	//test blink led
+	/*
+	   pinMode (0, OUTPUT) ;
+	   do
+	   {
+	   digitalWrite (0, HIGH) ; delay (1000) ;
+	   digitalWrite (0,  LOW) ; delay (1000) ;
+	   }while(true);
+	   */
 
-    engine = new motor_engine{make_motor_engine()};
-    engine->set_frequency(1600.);
+	// init engine
+	engine = new motor_engine{make_motor_engine()};
+	engine->set_frequency(1600.);
 
-    //create sensor
-    ultrasonic_sensor ultrasonic(ULTRASONIC_TRIGGER_PIN_WPI, ULTRASONIC_ECHO_PIN_WPI,
-            ULTRASONIC_BRAKE_LIGHT_PIN_WPI);
-    //init sensor
-    ultrasonic.init();
+	// init sensor
+	ultrasonic_sensor ultrasonic(ULTRASONIC_TRIGGER_PIN_WPI, ULTRASONIC_ECHO_PIN_WPI,
+			ULTRASONIC_BRAKE_LIGHT_PIN_WPI);
+	ultrasonic_sensor * ultrasonic_ptr = & ultrasonic;
+	ultrasonic.init();
 
-    bool ultrasonic_test = true;
-    if(ultrasonic_test){
-	    //debug time diff
-	    for(int i=0 ;i<100;i++){
-	    	ultrasonic.calc_distance() ;
-	    }
-	    for(int i=0 ;i<100;i++){
-	    	std::cout << ultrasonic.calc_distance() << std::endl;
-	    }
-	    return 0;
-    };
-    bool auto_movement = false;
-    if(auto_movement){
-	    std::cout << "driving forward" << std::endl;
-	    engine->set_speed(850);
-	    engine->forward();
-	    std::this_thread::sleep_for (std::chrono::milliseconds (500));
-	    engine->smooth_stop();
+	// periodically poll distance
+	std::thread sensor_thread(poll_distance,end_ptr,ultrasonic_ptr,engine);
 
-	    std::cout << "driving backwards" << std::endl;
-	    engine->set_speed(850);
-	    engine->backwards();
-	    std::this_thread::sleep_for (std::chrono::milliseconds (500));
-	    engine->smooth_stop();
+	//test thread is working
+	//std::thread sensor_thread(fun1,end_ptr);
 
-	    engine->release_engine();
-	    delete engine;
-    }else{
-	    char user_cmd = 0;
-	    initscr();
-	    cbreak();
-	    noecho();
-	    while((user_cmd = getch()) != 'x'){
-		    const int DIST_THRESHOLD = 10;
-		    /* TODO uncomment if(ultrasonic.calc_distance() < DIST_THRESHOLD && engine->get_direction() == direction::FORWARD){
-		       engine->emergency_stop();
-		       }*/
-		    switch (user_cmd){
-			    case 'e':{
-					     engine->emergency_stop();
-					     break;
-				     }
-			    case 'q':{
-					     engine->smooth_stop();
-					     break;
-				     }
-			    case 'w':{
-					     switch(engine->get_direction()){
-						     case direction::IN_PLACE_TURN_RIGHT:
-						     case direction::IN_PLACE_TURN_LEFT:
-							     engine->smooth_stop();
-							     engine->forward();
-							     engine->increase_speed();
-							     break;
-						     case direction::BACKWARDS:
-							     engine->decrease_speed();
-							     break;
-						     case direction::STOP:
-							     engine->forward();
-							     engine->increase_speed();
-							     break;
-						     case direction::FORWARD:
-							     engine->increase_speed();
-							     break;
-					     }
-					     break;
-				     }
-			    case 's':{
-					     switch(engine->get_direction()){
-						     case direction::IN_PLACE_TURN_RIGHT:
-						     case direction::IN_PLACE_TURN_LEFT:
-							     engine->smooth_stop();
-							     engine->backwards();
-							     engine->increase_speed();
-							     break;
-						     case direction::FORWARD:
-							     engine->decrease_speed();
-							     break;
-						     case direction::STOP:
-							     engine->backwards();
-							     engine->increase_speed();
-							     break;
-						     case direction::BACKWARDS:
-							     engine->increase_speed();
-							     break;
-					     }
-					     break;
-				     }
-			    case 'd':{
-					     switch(engine->get_direction()){
-						     case direction::IN_PLACE_TURN_RIGHT:
-							     engine->increase_speed();
-							     break;
-						     case direction::IN_PLACE_TURN_LEFT:
-							     engine->decrease_speed();
-							     break;
-						     case direction::FORWARD:
-						     case direction::BACKWARDS:
-							     engine->turn_right();
-							     break;
-						     case direction::STOP:
-							     engine->turn_in_place_right();
-							     engine->increase_speed();
-							     break;
-					     }
-					     break;
-				     }
-			    case 'a':{
-					     switch(engine->get_direction()){
-						     case direction::IN_PLACE_TURN_RIGHT:
-							     engine->decrease_speed();
-							     break;
-						     case direction::IN_PLACE_TURN_LEFT:
-							     engine->increase_speed();
-							     break;
-						     case direction::FORWARD:
-						     case direction::BACKWARDS:
-							     engine->turn_left();
-							     break;
-						     case direction::STOP:
-							     engine->turn_in_place_left();
-							     engine->increase_speed();
-							     break;
-					     }
-					     break;
-				     }
-				     //check distance
-			    case 'c':{
-					     //debug time diff
-					     ultrasonic.calc_distance();
-				     }
 
-			    default: break;
-		    }
 
-	    }
+	bool ultrasonic_test = false;
+	if(ultrasonic_test){
+		//debug time diff
+		for(int i=0 ;i<100;i++){
+			ultrasonic.calc_distance() ;
+		}
+		for(int i=0 ;i<100;i++){
+			std::cout << ultrasonic.calc_distance() << std::endl;
+		}
+		return 0;
+	};
 
-	    engine->smooth_stop();
-	    engine->release_engine();
-	    endwin();
-    }
+	bool auto_movement = false;
+	if(auto_movement){
+		std::cout << "driving forward" << std::endl;
+		engine->set_speed(850);
+		engine->forward();
+		std::this_thread::sleep_for (std::chrono::milliseconds (500));
+		engine->smooth_stop();
+
+		std::cout << "driving backwards" << std::endl;
+		engine->set_speed(850);
+		engine->backwards();
+		std::this_thread::sleep_for (std::chrono::milliseconds (500));
+		engine->smooth_stop();
+
+		engine->release_engine();
+		delete engine;
+		return 0;
+	}
+
+	// control buggy motor engine using wasd keys
+	char user_cmd = 0;
+	initscr();
+	cbreak();
+	noecho();
+	while((user_cmd = getch()) != 'x'){
+		/*
+		 * Check if remote control is enabled. Which is the case if our buggy is 
+		 * not below our safety distance threshold. 
+		 */
+		if(*end_ptr){
+			std::cout << "remote control disabled while distance is below safety threshold!" << std::endl;
+		}
+		else {
+
+		switch (user_cmd){
+			case 'e':{
+					 engine->emergency_stop();
+					 break;
+				 }
+			case 'q':{
+					 engine->smooth_stop();
+					 break;
+				 }
+			case 'w':{
+					 switch(engine->get_direction()){
+						 case direction::IN_PLACE_TURN_RIGHT:
+						 case direction::IN_PLACE_TURN_LEFT:
+							 engine->smooth_stop();
+							 engine->forward();
+							 engine->increase_speed();
+							 break;
+						 case direction::BACKWARDS:
+							 engine->decrease_speed();
+							 break;
+						 case direction::STOP:
+							 engine->forward();
+							 engine->increase_speed();
+							 break;
+						 case direction::FORWARD:
+							 engine->increase_speed();
+							 break;
+					 }
+					 break;
+				 }
+			case 's':{
+					 switch(engine->get_direction()){
+						 case direction::IN_PLACE_TURN_RIGHT:
+						 case direction::IN_PLACE_TURN_LEFT:
+							 engine->smooth_stop();
+							 engine->backwards();
+							 engine->increase_speed();
+							 break;
+						 case direction::FORWARD:
+							 engine->decrease_speed();
+							 break;
+						 case direction::STOP:
+							 engine->backwards();
+							 engine->increase_speed();
+							 break;
+						 case direction::BACKWARDS:
+							 engine->increase_speed();
+							 break;
+					 }
+					 break;
+				 }
+			case 'd':{
+					 switch(engine->get_direction()){
+						 case direction::IN_PLACE_TURN_RIGHT:
+							 engine->increase_speed();
+							 break;
+						 case direction::IN_PLACE_TURN_LEFT:
+							 engine->decrease_speed();
+							 break;
+						 case direction::FORWARD:
+						 case direction::BACKWARDS:
+							 engine->turn_right();
+							 break;
+						 case direction::STOP:
+							 engine->turn_in_place_right();
+							 engine->increase_speed();
+							 break;
+					 }
+					 break;
+				 }
+			case 'a':{
+					 switch(engine->get_direction()){
+						 case direction::IN_PLACE_TURN_RIGHT:
+							 engine->decrease_speed();
+							 break;
+						 case direction::IN_PLACE_TURN_LEFT:
+							 engine->increase_speed();
+							 break;
+						 case direction::FORWARD:
+						 case direction::BACKWARDS:
+							 engine->turn_left();
+							 break;
+						 case direction::STOP:
+							 engine->turn_in_place_left();
+							 engine->increase_speed();
+							 break;
+					 }
+					 break;
+				 }
+			default: break;
+		}
+		}
+	}
+
+	// join thread
+	sensor_thread.join();
+	engine->smooth_stop();
+	engine->release_engine();
+	endwin();
 }
