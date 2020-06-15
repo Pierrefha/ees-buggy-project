b83fe9f1a3a112ff159ab67e4a9819b937ae9df6
Leonhard Kipp <Leonhard.Kipp@web.de>
2020-06-15 13:16:01 +0200

    Added statistics

---
+++ test_data_ultrasonic/analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+#load dataframe from csv
+df = pd.read_csv("data.csv")
+
+print(df.describe().to_latex())
+
+# mean = df.mean()
+# diff = mean.sub([5, 10, 15, 20, 30, 50, 100])
+# diff.plot()
+# plt.show()
+#Plot is in file mean_diff.jpg
+
+# from scipy.stats import shapiro
+# for column in df:
+#     (w, p) = shapiro(df[column])
+#     df[column].hist()
+#     plt.show()
+#     if p < 0.05:
+#         print("Column", column, "is probably not normal distributed")
+#Output:
+# Column 5cm is probably not normal distributed
+# Column 10cm is probably not normal distributed
+# Column 15cm is probably not normal distributed
+# Column 20cm is probably not normal distributed
+# Column 30cm is probably not normal distributed
+# Column 50cm is probably not normal distributed
+# Column 100cm is probably not normal distributed
