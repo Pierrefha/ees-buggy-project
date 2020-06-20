10c3f013bc57748e1a09bf2d990150976c91f0d0
JensDennig <jens.dennigmann@t-online.de>
2020-06-20 19:48:19 +0200

    Update magnetic_sensor.h

--- inc/magnetic_sensor/magnetic_sensor.h
+++ inc/magnetic_sensor/magnetic_sensor.h
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 #define sensor_address	0x0d
 #define SET_RESET_REG	0x0b
 #define SETUP_REG		0x09
@@ -13,13 +15,21 @@ class magnetic_sensor{
 private:
 	//Filehandle für das Benutzen der WiringI2CLibrary
 	int fd;
-	int x;
-	int y;
-	int z;
+	int16_t x;
+	int16_t y;
+	int16_t z;
 		
 public:
+	//Konstruktor
 	magnetic_sensor();
+	//Getter für die Daten
+	int getX();
+	int getY();
+	int getZ();
 	
 	//void setup();
+	//Überprüft nach neuen Daten und fügt sie, wenn gegeben, in die Attribute ein
 	int check();
+	
+	double testDirection();
 };
