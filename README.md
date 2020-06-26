20cb30a34d2c69e85c16546d7101b5865bdab28f
JensDennig <58271170+JensDennig@users.noreply.github.com>
2020-06-26 12:33:43 +0200

    Cli for Tests

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 ## Build Command
 in the directory with the CMakeLists.txt
 cmake . && make && ./ees_buggy
+
+## Pinconfig
+Brackets show position on the Breadboard
+PI					Magnetic-sensor
+VCC 3V (C30)	->	VCC (B 30)
+GND	(C29)		->	GND (B 29)
+SCL	(C28)		->	SCL (B 28)
+SDA	(C27)			->	SDA (B 27)
+
+
+Motorhat	Ultrasonic-sensor
+Pin 0
+Pin 1
+Pin 2
--- main.cpp
+++ main.cpp
@@ -15,6 +15,9 @@
 #include <ultrasonic_sensor/ultrasonic_sensor.h>
 #include <chrono>
 #include <applications/wasd_control.h>
+// !!!! include string for cli comparison
+#inlude <string>
+// !!!!
 
 #ifdef TEST_ON
 #include "test_inc/test.h"
@@ -53,8 +56,8 @@ void signalHandler(int signum)
     exit(signum);
 }
 
-
-int main () {
+// !!!! added argc and argv for cli
+int main (int argc, char** argv) {
     // Csignal für Abbruch über STRG-C
     signal(SIGINT, signalHandler);
     //init wiringPi
@@ -79,7 +82,44 @@ int main () {
 #ifdef TEST_ON
     run_tests(engine, ultrasonic, _compass);
 #endif //TEST_ON
-
+	// !!!! processing of command line arguments
+	char tests[] = "GeneralTest";
+	char rotation[] = "Rotation";
+	char turns[] = "Drehungen";
+	char rectangle[] = "Rechteck";
+	char movePoint[] = "Punktbewegung";
+	char movePointsRectangle[] = "Punktbewegung_Rechteck";
+	char assistedMove[] = "UnterstuetzteBewegung";
+	char turn[] = "Drehen";
+	for(int i = 1; i < argc; i++){
+		
+		if(strcmp(argv[i],tests) == 0){
+			run_tests(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],rotation) == 0){
+			test_rotation_over_time(engine, _compass);
+		}
+		else if(strcmp(argv[i],turns) == 0){
+			test_turns(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],rectangle) == 0){
+			test_rectangle(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],movePoint) == 0){
+			test_move_to_point(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],movePointsRectangle) == 0){
+			test_move_rectangle_with_points(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],assistedMove) == 0){
+			test_move_with_direction_control(engine, ultrasonic, _compass);
+		}
+		else if(strcmp(argv[i],turn) == 0){
+			test_turn_by(engine, ultrasonic, _compass);
+		}
+	}
+	// !!!!
+	
 	wasd_controller = new wasd_control{};
 	wasd_controller->run(engine, ultrasonic, _compass);
 
